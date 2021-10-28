c81cf28c124656fed1c9476133b6b3f850e8ab6c
Agent FUD <agentfud@gmail.com>
2021-10-28 05:31:09 +0200

    version updated in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read_requirements():
 
 setup(
     name="artengine",
-    version="0.1.5",
+    version="0.1.6",
     author='Agent FUD',
     author_email='agentfud@gmail.com',
     description=DESCRIPTION,
