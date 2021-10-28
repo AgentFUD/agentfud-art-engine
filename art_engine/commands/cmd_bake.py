8e8835c1aa0b43016e04bbe553fcc406b1f042e2
Agent FUD <agentfud@gmail.com>
2021-10-28 23:14:21 +0200

    bake command updated, added build directory regenerate option

--- art_engine/commands/cmd_bake.py
+++ art_engine/commands/cmd_bake.py
@@ -1,51 +1,33 @@
 import click
+import shutil
+import os
 from art_engine.engine import ArtEngine
 import art_engine.appconfig as config
 
 
-@click.group()
-def cli():
+@click.command()
+@click.argument('type', type=click.Choice(['images', 'metadata', 'both']))
+@click.option('--quantity', type=int, required=True, help='How many I should generate')
+@click.option('--regenerate', type=bool)
+def cli(type, quantity, regenerate):
     """
     Bakes images and metadata files
     """
+    if regenerate:
+        shutil.rmtree('./build', ignore_errors=True)
+        for dir in config.project_template:
+            os.makedirs(
+                config.project_template[dir],
+                exist_ok=True,
+            )    
 
-
-@cli.command()
-@click.argument("quantity", type=int)
-def images(quantity):
-    """Builds images based on configuration"""
-    click.echo("Building images...")
     engine = ArtEngine(config)
     engine.setup_engine()
     engine.generate_dnas(quantity)
     engine.generate_sprite_configs()
-    for index, sprite_config in enumerate(engine.sprite_configs):
-        engine.build_image(index, sprite_config)
-
-
-@cli.command()
-@click.argument("quantity", type=int)
-def metadata(quantity):
-    """Builds metadata based on configuration"""
-    click.echo("Building metadata...")
-    engine = ArtEngine(config)
-    engine.setup_engine()
-    engine.generate_dnas(quantity)
-    engine.generate_sprite_configs()
-    for index, dna in enumerate(engine.dnas):
-        engine.build_metadata(index, dna)
-
-
-@cli.command()
-@click.argument("quantity", type=int)
-def all(quantity):
-    """Builds images based on configuration"""
-    click.echo("Building images and metadata...")
-    engine = ArtEngine(config)
-    engine.setup_engine()
-    engine.generate_dnas(quantity)
-    engine.generate_sprite_configs()
-    for index, sprite_config in enumerate(engine.sprite_configs):
-        engine.build_image(index, sprite_config)
-    for index, dna in enumerate(engine.dnas):
-        engine.build_metadata(index, dna)
+    if type in ['images', 'both']:
+        for index, sprite_config in enumerate(engine.sprite_configs):
+            engine.build_image(index, sprite_config)
+    if type in ['metadata', 'both']:
+        for index, dna in enumerate(engine.dnas):
+            engine.build_metadata(index, dna)
